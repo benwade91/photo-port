db02dd6bf70f411f2a64eaea57ea41ecd195a038
benwade91 <70988204+benn925@users.noreply.github.com>
2021-04-20 09:34:04 -0700

    added tests for about

---
+++ photo-port/src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+afterEach(cleanup);
+
+describe('Nav component', () => {
+    // First Test
+    it('renders', () => {
+      render(<Nav />);
+    })
+  
+  })
